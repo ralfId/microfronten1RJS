b3082c2b258dc1038e11c915f7d17a79b276fab9
rafael.diego <rafael.diego@onelinkbpo1.onmicrosoft.com>
2022-06-10 10:02:23 -0600

    change default container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 
 
 function App() {
-setTimeout(
-  ()=>{
-    return (
-      < >
-        <div className="container">
-          <h1>Microfronten 1</h1>
-          <p className="display-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-          </p>
-        </div>
-      </>
-    );
-  },2000
-);
+
+  localStorage.setItem("mfront1", "Esto es una variable en localstorage del microfrontend 1");
+  return (
+    < >
+      <div className="container">
+        <h1>Microfronten 1</h1>
+        <p className="display-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        </p>
+      </div>
+    </>
+  );
 
   
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const root = createRoot(document.getElementById('root'));
+
 
 window.renderMfe1 = (containerId, history) => {
-root.render(<App history={history} />);
-
-  //serviceWorker.unregister();
+  const root = createRoot(document.getElementById(containerId));
+  root.render(<App history={history} />);
+  serviceWorker.unregister();
 };
 
 window.unmountMfe1 = containerId => {
+  const root = createRoot(document.getElementById(containerId));
   root.unmount(containerId);
 };
 
 if (!document.getElementById('Mfe1-container')) {
+  const root = createRoot(document.getElementById("root"));
   root.render(<App />);
-  //serviceWorker.unregister();
+  serviceWorker.unregister();
 }
