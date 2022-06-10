65fcb9f593136ea46d1d4ffc5273f69ea77b1b26
rafael.diego <rafael.diego@onelinkbpo1.onmicrosoft.com>
2022-06-09 18:31:58 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+setTimeout(
+  ()=>{
+    return (
+      < >
+        <div className="container">
+          <h1>Microfronten 1</h1>
+          <p className="display-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          </p>
+        </div>
+      </>
+    );
+  },2000
+);
+
+  
 }
 
 export default App;
